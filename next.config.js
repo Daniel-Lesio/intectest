5f9c2f5c52d45ba72269e6fba3b04cc4893da446
daniel-lesio <neoonepl@gmail.com>
2021-01-18 09:46:40 +0100

    fs

--- next.config.js
+++ next.config.js
@@ -2,4 +2,14 @@ module.exports = {
     images: {
       domains: ['images.pexels.com'],
     },
+    webpack: (config, { isServer }) => {
+        // Fixes npm packages that depend on `fs` module
+        if (!isServer) {
+          config.node = {
+            fs: 'empty'
+          }
+        }
+    
+        return config
+    }
   }
