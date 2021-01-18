2462c5c5777f8ace3cd66f02aa6048ce4444d320
daniel-lesio <neoonepl@gmail.com>
2021-01-18 07:14:57 +0100

    abc

--- Components/Reference/Reference.tsx
+++ Components/Reference/Reference.tsx
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from 'react';
 import styled from 'styled-components';
-import {motion,AnimatePresence} from 'framer-motion'
+import {motion} from 'framer-motion'
 import { Paragraph } from '../../styledComponents/styledComponents';
 const Reference = (props) => {
     const [white  ,setWhite] = useState(false)   
@@ -25,10 +25,10 @@ const Reference = (props) => {
     return (
         <ReferenceComp>
             <Descriptions white={white}>
-                <h3 style={{fontWeight : 900}}>Project name: <span style={{fontWeight:'400', textTransform  : 'capitalize!important' ,paddingLeft : '10px' }}>{props.obj.name}</span></h3>
-                <h3 style={{fontWeight : 900}}>Place of project: <span style={{fontWeight:'400', textTransform  : 'capitalize!important' ,paddingLeft : '10px' }}>{props.obj.place}</span></h3>
-                <h3 style={{fontWeight : 900}}>CAPACITY :<span style={{fontWeight:'400', textTransform  : 'capitalize!important' ,paddingLeft : '10px' }}>{props.obj.capacity}</span></h3>
-                <h3 style={{fontWeight : 900}}>DATE OF COMPLETION: <span  style={{fontWeight:400 , textTransform  : 'capitalize!important',paddingLeft : '10px'}}>{props.obj.completion}</span></h3>
+                <h3 style={{fontWeight : 900}}>Project name: <span style={{fontWeight:400, textTransform  : 'capitalize' ,paddingLeft : '10px' }}>{props.obj.name}</span></h3>
+                <h3 style={{fontWeight : 900}}>Place of project: <span style={{fontWeight:400, textTransform  : 'capitalize' ,paddingLeft : '10px' }}>{props.obj.place}</span></h3>
+                <h3 style={{fontWeight : 900}}>CAPACITY :<span style={{fontWeight:400, textTransform  : 'capitalize' ,paddingLeft : '10px' }}>{props.obj.capacity}</span></h3>
+                <h3 style={{fontWeight : 900}}>DATE OF COMPLETION: <span  style={{fontWeight:400 , textTransform  : 'capitalize',paddingLeft : '10px'}}>{props.obj.completion}</span></h3>
                 <Arrow onClick={()=>slide()} >
                         <svg width="19" height="16" fill="none">
                             <path fillRule="evenodd" clipRule="evenodd" d="M18.7071 8.70737C19.0976 8.31684 19.0976 7.68368 18.7071 7.29315L12.3432 0.92913C11.9527 0.538602 11.3195 0.538596 10.929 0.929117C10.5385 1.31964 10.5385 1.9528 10.929 2.34333L15.5858 7.00023L6.00933 7.00014C6.00622 7.00011 6.00311 7.00009 6 7.00009L1.50456 7.00009L1.00001 7.00009L1 7.00009C0.447715 7.00009 0 7.44781 0 8.00009C0 8.55238 0.447715 9.00009 1 9.00009H1.4995L15.5858 9.00023L10.9289 13.657C10.5384 14.0476 10.5383 14.6807 10.9289 15.0713C11.3194 15.4618 11.9526 15.4618 12.3431 15.0713L18.7071 8.70737Z" fill="#EB7700"/>
@@ -69,8 +69,10 @@ const ReferenceComp = styled.article`
     z-index : 4;   
 `;
 
-
-const Descriptions = styled.div`
+interface DescProps {
+    white : Boolean
+}
+const Descriptions = styled.div<DescProps>`
 z-index :5;
 display :flex;
 color : ${props=>props.white ? 'white!important': 'black!important'};
