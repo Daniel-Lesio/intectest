a2fcc2ae49b1e291446f683f21efb60e1aab24f0
daniel-lesio <neoonepl@gmail.com>
2021-01-18 07:40:10 +0100

    abc

--- next.config.js
+++ next.config.js
@@ -2,4 +2,10 @@ module.exports = {
     images: {
       domains: ['images.pexels.com'],
     },
+    webpack: (config, options) => {
+      config.node = {
+        fs: 'empty',
+        path : 'empty'
+      };
+    },
   }
