787e0cf7ede3999f453f61fcda16527ea5261d81
daniel-lesio <neoonepl@gmail.com>
2021-01-22 11:20:55 +0100

    x

--- Components/Certificates/Certificates.jsx
+++ Components/Certificates/Certificates.jsx
@@ -6,31 +6,17 @@ const Certificates = () => {
         
     <section id="certificate">
       <h1>CERTIFICATE</h1>
-      <div className="container" style={{display:'flex',justifyContent:'space-between'}}>
+      <div className="container" style={{display:'flex',justifyContent:'center'}}>
         <Image src='/assets/cert1.png'
         width={80}
         height={120}
         />
+        <div style={{width: '100px',height : '100px'}}></div>
         <Image src='/assets/cert2.png'
         width={80}
         height={120}
         />
-        <Image src='/assets/cert1.png'
-        width={80}
-        height={120}
-        />
-        <Image src='/assets/cert2.png'
-        width={80}
-        height={120}
-        />
-        <Image src='/assets/cert1.png'
-        width={80}
-        height={120}
-        />
-        <Image src='/assets/cert2.png'
-        width={80}
-        height={120}
-        />
+        
       </div>
     </section>
     );
