1877d0d1078f5778fb8ee037f87fa1824c4d696b
daniel-lesio <neoonepl@gmail.com>
2021-02-11 10:54:56 +0100

    x

--- Layout/Layout.tsx
+++ Layout/Layout.tsx
@@ -4,15 +4,19 @@ import SocialsComp from '../Components/SocialsComp/SocialsComp'
 import ContactComp from '../Components/ContactComp/ContactComp' 
 import FooterComp from '../Components/FooterComp/FooterComp'
 import Notify from '../Components/Notify/Notify'
+import Head from 'next/head'
 import {RecoilRoot} from 'recoil'
 const Layout: React.FunctionComponent<LayoutProps> = ({children}) => {
     return ( 
+        <>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
         <div style={{overflowX : 'hidden'}}>
             <Header/>
             {children}
             <SocialsComp/>
             <ContactComp/>
             <FooterComp/>
+        </>
             
         </div>
      );
